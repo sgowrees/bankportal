64febc142bac0fe06a3a7881a36daee4348826a8
Sivan <Sivan.g1307@gmail.com>
2026-06-19 15:20:00 -0400

    init

---
+++ backend/src/test/java/com/app/bankportal/BankportalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.bankportal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankportalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
